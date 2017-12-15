ebc99161bfb57cbeff98de781e44b7a9c57501e9
KsushaSeliv <KsenijaSeliv@yandex.ru>
2017-12-15 17:16:52 +0300

    Update factorial(unittest).py

--- factorial(unittest).py
+++ factorial(unittest).py
@@ -1,1 +1,20 @@
+import unittest
+import factorial
 
+class TestFactorialMethods(unittest.TestCase):
+  def test_normal_values(self):
+   self.assertEqual(fact(2),2)
+   self.assertEqual(fact(7),5040)
+   self.assertEqual(fact(9),362880)
+  def test_except_ValueError(self):
+   with self.assertRaises(ValueError):
+    fact(-1)
+   with self.assertRaises(ValueError):
+    fact(-0.5)
+  def test_except_TypeError(self):
+   with self.assertRaises(TypeError):
+    fact(0.2)
+   with self.assertRaises(TypeError):
+    fact([0,2,7,9])
+   with self.assertRaises(TypeError):
+    fact('listik')
